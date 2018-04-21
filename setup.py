6556d5098b08635a35f75487a905f421c56c611b
Pietro Battiston <me@pietrobattiston.it>
2018-04-21 16:06:17 +0200

    cython profile

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+from Cython.Compiler.Options import get_directive_defaults
+
+directive_defaults = get_directive_defaults()
+directive_defaults['linetrace'] = True
+directive_defaults['binding'] = True
+
 extensions = [
-   Extension("colors_or_cyt", ["colors_or_cyt.pyx"])
+   Extension("colors_or_cyt", ["colors_or_cyt.pyx"],
+             define_macros=[('CYTHON_TRACE', '1')])
 ]
 
 setup(
