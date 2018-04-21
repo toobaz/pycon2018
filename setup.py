3195b5d3eace3bfed78b3de2ed7c6438610663e8
Pietro Battiston <me@pietrobattiston.it>
2018-04-21 16:03:59 +0200

    distutils.extension

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
+
+extensions = [
+   Extension("colors_or_cyt", ["colors_or_cyt.pyx"])
+]
 
 setup(
    name = 'colors',
-   ext_modules = cythonize('colors_or_cyt.pyx')
+   ext_modules = cythonize(extensions)
    )
