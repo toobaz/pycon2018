7e7ff29a4665dc7cdd4fff1a659daad27624adc5
Pietro Battiston <me@pietrobattiston.it>
2018-04-21 15:34:11 +0200

    Versione Python

---
+++ colors_or.py
@@ -0,0 +1,30 @@
+def colors_or(left, right):
+    N = len(left)
+
+    todo = [True] * N
+    
+    for idx in range(N):
+        if not todo[idx]:
+            continue
+        
+        connected = {idx}
+        prev_size = 1
+        
+        for it in range(N):
+            r_cols = {right[idx] for idx in connected}
+            connected = {i for i in range(N) if right[i] in r_cols}
+            
+            l_cols = {left[idx] for idx in connected}
+            connected = {i for i in range(N) if left[i] in l_cols}
+            
+            size = len(connected)
+            if size == prev_size:
+                l_col = l_cols.pop()
+                r_col = r_cols.pop()
+                for i in connected:
+                    left[i] = l_col
+                    right[i] = r_col
+                    todo[i] = False
+                break
+            prev_size = size
+    return left
